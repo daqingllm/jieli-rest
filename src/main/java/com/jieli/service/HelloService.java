d300628f084c7c93059ef39c475d9e94202adb30
Liming Liu <liming.liu@dianping.com>
2014-03-08 23:21:41 +0800

    hello service

---
+++ src/main/java/com/jieli/service/HelloService.java
@@ -0,0 +1,38 @@
+package com.jieli.service;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: liming_liu
+ * Date: 14-3-8
+ * Time: 下午11:02
+ * To change this template use File | Settings | File Templates.
+ */
+@Path("/hello")
+public class HelloService {
+    // This method is called if TEXT_PLAIN is request
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sayPlainTextHello() {
+        return "Hello Jersey";
+    }
+
+    // This method is called if XML is request
+    @GET
+    @Produces(MediaType.TEXT_XML)
+    public String sayXMLHello() {
+        return "<?xml version=\"1.0\"?>" + "<hello> Hello Jersey" + "</hello>";
+    }
+
+    // This method is called if HTML is request
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public String sayHtmlHello() {
+        return "<html> " + "<title>" + "Hello Jersey" + "</title>"
+                + "<body><h1>" + "Hello Jersey" + "</body></h1>" + "</html> ";
+    }
+}
