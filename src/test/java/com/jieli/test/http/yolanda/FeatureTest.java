c42e252bc4055ac62a44622ea8410d94cb57655f
meijia.lv <meijia.lv@gmail.com>
2014-03-31 17:50:20 +0800

    分割线前面接口测试都已通过，dao测试都已通过，分割线后面几个接口还没测

--- src/test/java/com/jieli/test/http/yolanda/FeatureTest.java
+++ src/test/java/com/jieli/test/http/yolanda/FeatureTest.java
@@ -4,12 +4,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jieli.feature.help.entity.HelpComment;
 import com.jieli.feature.help.entity.HelpInfo;
+import com.jieli.feature.vote.entity.VoteInfo;
 import org.apache.http.client.fluent.Request;
 import org.apache.http.client.fluent.Response;
 import org.apache.http.entity.ContentType;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Date;
 
 /**
@@ -86,8 +88,49 @@ public class FeatureTest {
         System.out.println(response.returnContent().asString());
     }
 
+    //分割线
     @Test
-    public void addTop() {
+    public void addTop() throws IOException {
+        String helpId = "5338115f30043866e28ac0b2";
+        Integer index = 1;
+        Response response = Request.Post("http://localhost:8080/rest/feature/help/detail/comment/top" +
+                "?helpId=5338115f30043866e28ac0b2")
+                .setHeader("Cookie", "u=5336bb6f3004cc09f49432e5")
+                .bodyString(helpId + index.toString(), ContentType.APPLICATION_JSON)
+                .execute();
+        System.out.println(response.returnContent().asString());
+    }
 
+    @Test
+    public void getVoteList() throws IOException {
+        Response response = Request.Post("http://localhost:8080/rest/feature/vote")
+                .setHeader("Cookie", "u=5336bb6f3004cc09f49432e5")
+                .execute();
+        System.out.println(response.returnContent().asString());
+    }
+
+    @Test
+    public void getVoteInfo() throws IOException {
+        Response response = Request.Post("http://localhost:8080/rest/feature/vote/detail?voteId=533807ec300406d49b957887")
+                .setHeader("Cookie", "u=5336bb6f3004cc09f49432e5")
+                .execute();
+        System.out.println(response.returnContent().asString());
+    }
+
+    @Test
+    public void addVote() throws IOException {
+        VoteInfo voteInfo = new VoteInfo();
+        voteInfo.setAssociationId("5337af643004e0056052bd5a");
+        voteInfo.setDeadLine(new Date("2014-05-01"));
+        voteInfo.setMultiple(false);
+        voteInfo.setTitle("喜欢的颜色");
+        voteInfo.setOptions(Arrays.asList("红", "黄", "蓝", "绿"));
+        voteInfo.setDescription("喜欢什么颜色？");
+        ObjectMapper mapper = new ObjectMapper();
+        Response response = Request.Post("http://localhost:8080/rest/feature/vote/addvote")
+                .setHeader("Cookie", "u=5336bb6f3004cc09f49432e5")
+                .bodyString(mapper.writeValueAsString(voteInfo), ContentType.APPLICATION_JSON)
+                .execute();
+        System.out.println(response.returnContent().asString());
     }
 }
