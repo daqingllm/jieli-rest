99e1fba8cdfa1e726c4549da90e3873164164f85
liangjun.zhong <zhongliangjun1@gmail.com>
2014-04-01 21:26:36 +0800

    comment done

---
+++ src/main/java/com/jieli/comment/Comment.java
@@ -0,0 +1,31 @@
+package com.jieli.comment;
+
+import com.jieli.mongo.Model;
+
+import java.util.Date;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: liangjun.zhong
+ * Date: 14-4-1
+ * Time: PM7:47
+ * To change this template use File | Settings | File Templates.
+ */
+public class Comment extends Model {
+    private static final long serialVersionUID = -3459753602651398540L;
+
+    public String topicId; // 评论内容id
+    public String topicType; // 被评论内容类型
+
+    public String commentUserId;  // 回复者id
+    public String commentedUserId; // 被回复者id
+
+    public String content;
+
+    public Date addTime;
+
+
+    public CommentUserInfo commentUserInfo; // 评论者详细信息，用于展示
+
+
+}
---
+++ src/main/java/com/jieli/comment/CommentUserInfo.java
@@ -0,0 +1,18 @@
+package com.jieli.comment;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Author: liangjun.zhong
+ * Date: 14-4-1
+ * Time: PM8:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class CommentUserInfo {
+
+    public String userId;
+    //头像
+    public String userFace;
+
+    public String name;
+
+}
