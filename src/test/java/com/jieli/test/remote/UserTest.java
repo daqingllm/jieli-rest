a31ce201c73c39439437433c6d376acee6f27094
meijia.lv <meijia.lv@gmail.com>
2014-04-03 17:55:39 +0800

    feature test

--- src/test/java/com/jieli/test/remote/UserTest.java
+++ src/test/java/com/jieli/test/remote/UserTest.java
@@ -13,7 +13,7 @@ public class UserTest {
     @Test
     public void testLoadUser() throws IOException {
         Response response = Request.Get("http://162.243.151.219:8080/jieli-1.0-SNAPSHOT/rest/user?userId=533c09e5e4b05bd824aeda58")
-                .addHeader("Cookie", "u=533bfca63a6e26a4f86e916d")
+                .addHeader("Cookie", "u=533c09e5e4b05bd824aeda58")
                 .execute();
 
         System.out.println(response.returnContent().asString());
