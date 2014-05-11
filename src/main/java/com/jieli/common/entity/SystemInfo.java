a5a981e5a5fa6f47b396ad322c27b1f7114baed5
Liming Liu <liming.liu@dianping.com>
2014-05-11 16:03:56 +0800

    ..

---
+++ src/main/java/com/jieli/common/entity/SystemInfo.java
@@ -0,0 +1,17 @@
+package com.jieli.common.entity;
+
+import com.jieli.mongo.Model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: liming_liu
+ * Date: 14-5-11
+ * Time: 下午3:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class SystemInfo extends Model {
+
+    public String aboutJieli;
+
+    public String aboutSystem;
+}
--- src/main/java/com/jieli/service/SystemService.java
+++ src/main/java/com/jieli/service/SystemService.java
@@ -4,13 +4,13 @@ import com.jieli.association.AssociationDAO;
 import com.jieli.common.dao.FeedbackDAO;
 import com.jieli.common.entity.Feedback;
 import com.jieli.common.entity.ResponseEntity;
+import com.jieli.common.entity.SystemInfo;
+import com.jieli.mongo.BaseDAO;
 import com.jieli.util.IdentityUtils;
 import com.sun.jersey.spi.resource.Singleton;
+import org.apache.commons.lang.StringUtils;
 
-import javax.ws.rs.CookieParam;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -28,6 +28,8 @@ public class SystemService {
     FeedbackDAO feedbackDAO = new FeedbackDAO();
 
     AssociationDAO associationDAO = new AssociationDAO();
+
+    BaseDAO<SystemInfo> systemDAO = new BaseDAO<SystemInfo>("systeminfo", SystemInfo.class);
 
     @POST
     @Path("/feedback")
@@ -48,4 +50,83 @@ public class SystemService {
         responseEntity.code = 200;
         return Response.status(200).entity(responseEntity).build();
     }
+
+    @POST
+    @Path("/aboutSystem")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Response upsertSystemInfo(@CookieParam("u")String sessionId, String content) {
+        if (!IdentityUtils.isSuper(sessionId)) {
+            return Response.status(403).build();
+        }
+        ResponseEntity responseEntity = new ResponseEntity();
+        if (StringUtils.isEmpty(content)) {
+            responseEntity.code = 10;
+            responseEntity.msg = "缺少内容";
+            return Response.status(200).entity(responseEntity).build();
+        }
+        SystemInfo info = systemDAO.findOne("{}");
+        info.aboutSystem = content;
+        systemDAO.save(info);
+
+        responseEntity.code = 200;
+        return Response.status(200).entity(responseEntity).build();
+    }
+
+    @GET
+    @Path("/aboutSystem")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Response loadSystemInfo(@CookieParam("u")String sessionId) {
+        if (!IdentityUtils.isValidate(sessionId)) {
+            return Response.status(403).build();
+        }
+        ResponseEntity responseEntity = new ResponseEntity();
+        String content = "尚无系统信息";
+        SystemInfo info = systemDAO.findOne("{}");
+        if (StringUtils.isNotEmpty(info.aboutSystem)) {
+            content = info.aboutSystem;
+        }
+        responseEntity.code = 200;
+        responseEntity.body = content;
+        return Response.status(200).entity(responseEntity).build();
+    }
+
+    @POST
+    @Path("/aboutJieli")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Response upsertJieliInfo(@CookieParam("u")String sessionId, String content) {
+        if (!IdentityUtils.isSuper(sessionId)) {
+            return Response.status(403).build();
+        }
+        ResponseEntity responseEntity = new ResponseEntity();
+        if (StringUtils.isEmpty(content)) {
+            responseEntity.code = 10;
+            responseEntity.msg = "缺少内容";
+            return Response.status(200).entity(responseEntity).build();
+        }
+        SystemInfo info = systemDAO.findOne("{}");
+        info.aboutJieli = content;
+        systemDAO.save(info);
+
+        responseEntity.code = 200;
+        return Response.status(200).entity(responseEntity).build();
+    }
+
+    @GET
+    @Path("/aboutJieli")
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
+    public Response loadJieliInfo(@CookieParam("u")String sessionId) {
+        if (!IdentityUtils.isValidate(sessionId)) {
+            return Response.status(403).build();
+        }
+        ResponseEntity responseEntity = new ResponseEntity();
+        String content = "尚无系统信息";
+        SystemInfo info = systemDAO.findOne("{}");
+        if (StringUtils.isNotEmpty(info.aboutJieli)) {
+            content = info.aboutJieli;
+        }
+        responseEntity.code = 200;
+        responseEntity.body = content;
+        return Response.status(200).entity(responseEntity).build();
+    }
+
 }
